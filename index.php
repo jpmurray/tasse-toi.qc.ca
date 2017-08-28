3b8d2eb7215d72318ccc3789a295876530a5eaa5
Jean-Philippe Murray <jeanphilippe@pasunecompagnie.com>
2017-08-28 09:15:01 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:fb="http://www.facebook.com/2008/fbml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta property="og:title" content="Tasse-toi! 4 étapes faciles pour mieux vivre dans le métro de Montréal!"/>
+    <meta property="og:type" content="cause"/>
+    <meta property="og:url" content="http://tasse-toi.qc.ca/"/>
+    <meta property="og:site_name" content="Tasse-toi! "/>
+    <meta property="fb:admins" content="510485448" />
+<title>Tasse-toi! 4 étapes faciles pour mieux vivre dans le métro de Montréal!</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+</head>
+<body>
+<div id="fb-root"></div> 
+<script> 
+  window.fbAsyncInit = function() { 
+    FB.init({ 
+      appId  : '367940630288', 
+      status : true, // check login status 
+      cookie : true, // enable cookies to allow the server to access the session 
+      xfbml  : true  // parse XFBML 
+    }); 
+  }; 
+  (function() { 
+    var e = document.createElement('script'); 
+    e.src = document.location.protocol + '//connect.facebook.net/fr_FR/all.js'; 
+    e.async = true; 
+    document.getElementById('fb-root').appendChild(e); 
+  }()); 
+</script>
+<div id="titre"> <img src="images/tasse-toi.png" /> </div>
+<div id="ligneBlanche">
+  <div id="menu"><strong> <a href="#" class="menuBleu">
+    <!--Lire le blogue</a> <font size="-1">- -->
+    <a href="index_en.php" class="menuBleu">We also speak english!</a></font></strong></div>
+</div>
+<div id="conteneur">
+  <div id="sousTitre"> <img src="images/sousTitre.png" width="934" height="32" style="margin-top:25px;"/> </div>
+  <div id="one" class="contentslider">
+    <div class="cs_wrapper">
+      <div class="cs_slider">
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape1.png" style="margin-left:30px;"/></div>
+          <div class="texteEtape">
+            <p><strong>Les petits indicateurs sous forme de flèches jaunes</strong> que vous pouvez apercevoir sur les quais de certaines stations <strong>ne sont pas des cibles</strong>. Elles n'indiquent pas l’endroit où vous placer afin d’obtenir le meilleur endroit pour entrer au plus vite dans le wagon. Bien au contraire! Elles vous indiquent où <strong>dégager le chemin afin de faciliter la sortie des passagers déjà dans le wagon</strong> qui veulent descendre à la station.</p>
+            <p>Si il y a de l’espace pour sortir sans encombrement, le wagon se videra  plus rapidement et vous pourrez entrer tout aussi rapidement et sans problème!</p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape2.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+             <p>Il est totalement <strong>inutile de charger</strong> à l’intérieur d’un wagon<strong> si les occupants actuels ne sont pas tous sortis</strong>! En effet, vous ne faites qu'empêcher les gens de sortir agréablement et rapidement du wagon.</p>
+            <p>Dans la même logique que l'étape 1: si les gens sortent plus rapidement du wagon, vous y entrez tous aussi rapidement!</p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape3.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+            <p>Une fois <strong>à l'intérieur du wagon</strong>, soyez logique! S'il y a de l'espace plus loin, <strong>ne restez pas devant la porte</strong>! Vous bloquez l'entrée et empêchez le flot des passagers! </p>
+        	<p>Plus vite les gens auront  fait leur entrée ou sortie du wagon, celui-ci pourra partir et la qualité du service s'en trouvera améliorée!</p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape4.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+           <p>Si vous êtes témoin du fait que <strong>d'autres passagers qui contreviennent aux conventions  précédentes</strong>, ou tout autre acte qui rendrait  désagréable l'usage du transport en commun dans le réseau de la STM, <strong>ne restez pas passif</strong>! Expliquez gentiment que vous devez passer, que si vous ne pouvez sortir la personne ne sera pas en mesure d'entrer et, le tout, avec le sourire!</p>
+       	  <p>Vous pouvez aussi:</p>
+    <ul>
+        	  <li>Imprimer et <strong><a href="infraction_fr.pdf" target="_blank" onClick="javascript: pageTracker._trackPageview('/downloads/infractionFR');">distribuer des constats d'infraction au civisme</a></strong> dans le métro;</li>
+      <li>Joindre le <strong><a href="http://facebook.com/tassetoi" target="_blank">groupe Facebook </a>et passez le mot</strong>!</li>
+        </ul>
+        	<p>Avec un minimum d'effort, nous pouvons tous faire notre part pour rendre notre utilisation du métro plus agréable!</p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+      </div>
+      <!-- End cs_slider -->
+    </div>
+    <!-- End cs_wrapper -->
+  </div>
+  <!-- End contentslider -->
+  <div id="footer"> 
+    <p><fb:like href="http://www.facebook.com/tassetoi" show-faces="true" colorscheme="light" font="verdana"></fb:like></p>
+	<p><g:plusone size="tall" annotation="inline" href="http://tasse-toi.qc.ca"></g:plusone></p>
+    <p><b><a href="http://facebook.com/tassetoi" target="_blank">Facebook</a></b> - <a href="mailto:info@tasse-toi.qc.ca">Contact</a><br />
+      &copy; 2010 -2012  <a href="http://pasunecompagnie.com" target="_blank">Pas une compagnie</a></p>
+  </div>
+</div>
+<!-- Site JavaScript -->
+<script type="text/javascript"
+			src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.ennui.contentslider.js"></script>
+<script type="text/javascript">
+			$(function() {
+				$('#one').ContentSlider({
+					width : '935px',
+					height : '200px',
+					speed : 800,
+					easing : 'easeOutQuad'
+				});
+			});
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12779558-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+  window.___gcfg = {lang: 'fr'};
+
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+</body>
+</html>
---
+++ index_en.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta property="og:title" content="Tasse-toi! Keeping the subway doorways clear..."/>
+    <meta property="og:type" content="cause"/>
+    <meta property="og:url" content="http://tasse-toi.qc.ca/"/>
+    <meta property="og:site_name" content="Tasse-toi! "/>
+    <meta property="fb:admins" content="510485448" />
+<title>Tasse-toi! Keeping the subway doorways clear...</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/jquery.ennui.contentslider.css" rel="stylesheet" type="text/css" media="screen,projection" />
+</head>
+<body>
+<div id="fb-root"></div> 
+<script> 
+  window.fbAsyncInit = function() { 
+    FB.init({ 
+      appId  : '367940630288', 
+      status : true, // check login status 
+      cookie : true, // enable cookies to allow the server to access the session 
+      xfbml  : true  // parse XFBML 
+    }); 
+  }; 
+  (function() { 
+    var e = document.createElement('script'); 
+    e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js'; 
+    e.async = true; 
+    document.getElementById('fb-root').appendChild(e); 
+  }()); 
+</script>
+<div id="titre"> <img src="images/tasse-toi_en.png" /> </div>
+<div id="ligneBlanche">
+  <div id="menu"><strong> <a href="#" class="menuBleu">
+    <!--Lire le blogue</a> <font size="-1">- -->
+  <a href="index.php" class="menuBleu">Nous parlons aussi français!</a></font></strong></div>
+</div>
+<div id="conteneur">
+  <div id="sousTitre" align="center"> <img src="images/sousTitre_EN.png" width="634" height="31" style="margin-top:25px;"/> </div>
+  <div id="one" class="contentslider">
+    <div class="cs_wrapper">
+      <div class="cs_slider">
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape1.png" style="margin-left:30px;"/></div>
+          <div class="texteEtape">
+            <p> <strong>The small yellow arrows</strong> you can see on the platforms of some stations <strong>are not targets.</strong> They don't indicate where you can stand to get on the car more quickly. Quite the contrary! The yellow lines indicate the boundary zone to keep clear in order to <strong>allow the easy departure of passengers already in the car</strong> getting off at your station.</p>
+            <p>Keeping a low-congestion area allows people to empty off the car more quickly, allowing you to get on board just as quickly and smoothly! </p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape2.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+            <p> It is totally <strong>unnecessary to board</strong> a car <strong>before the current occupants have disembarked!</strong> This only prevents people from easily getting off the car, and prevents the car from moving on quickly. In the same logic as in Step 1, if people come off the car faster, you can get on more quickly. </p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape3.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+           <p>Once <strong>inside the car,</strong> be logical! If there's space farther down the car, <strong>don't stand just inside the door!</strong> By doing this, you block the entrance and prevent the flow of traffic!</p>
+        	<p>The sooner people have gotten off and on the car, the sooner it will go ...and the quality of service will improve! </p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+        <div class="cs_article">
+          <div class="titreEtape"><img src="images/etape4.png" style="padding-left:30px;"/></div>
+          <div class="texteEtape">
+          <p> If you witness<strong> other passengers violating Steps 1, 2 or 3,</strong> or any other act which makes your use of the STM network unpleasant, <strong>speak up! </strong>Explain politely that you must get off the car; if you can't, the other person won't be able to get on ...and say it all with a smile!  </p>
+   	    <p> You can also: </p>
+<ul>
+        	  <li> Print and<strong><a href="infraction_fr.pdf" target="_blank"> submit reports of improper behaviour</a></strong> in the subway <em>(in french only for now</em>);</li>
+      <li> Join the <strong><a href="http://facebook.com/tassetoi" target="_blank">Facebook group</a> and spread the word!</strong></li>
+        </ul>
+       	<p> With a little effort we can all do our part to make our use of the Metro easy and more enjoyable! </p>
+          </div>
+        </div>
+        <!-- End cs_article -->
+      </div>
+      <!-- End cs_slider -->
+    </div>
+    <!-- End cs_wrapper -->
+  </div>
+  <!-- End contentslider -->
+  <div id="footer">
+  <p><fb:like href="http://www.facebook.com/tassetoi" show-faces="true" colorscheme="light" font="verdana"></fb:like></p>
+  <p><g:plusone size="tall" annotation="inline" href="http://tasse-toi.qc.ca"></g:plusone></p>
+  <p><b><a href="http://facebook.com/tassetoi" target="_blank">Facebook</a></b> - <a href="mailto:info@tasse-toi.qc.ca">Contact</a><br />
+  	&copy; 2010 -2012  <a href="http://pasunecompagnie.com" target="_blank">Pas une compagnie</a></p>
+  </div>
+</div>
+<!-- Site JavaScript -->
+<script type="text/javascript"
+			src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/jquery.ennui.contentslider.js"></script>
+<script type="text/javascript">
+			$(function() {
+				$('#one').ContentSlider({
+					width : '935px',
+					height : '200px',
+					speed : 800,
+					easing : 'easeOutQuad'
+				});
+			});
+</script>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-12779558-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+<script type="text/javascript">
+  (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+  })();
+</script>
+</body>
+</html>
